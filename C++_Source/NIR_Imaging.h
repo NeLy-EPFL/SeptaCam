78b10cce2c1e40faaed9bf9477129db2101763d1
Sibo Wang <sibowsb@gmail.com>
2021-06-10 11:53:33 +0200

    format improvement

--- C++_Source/NIR_Imaging.h
+++ C++_Source/NIR_Imaging.h
@@ -1,19 +1,20 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 
-int    cameras_connect(); 
-void   cameras_disconnect();
-std::array<long int, 4> cameras_getROI(int index); 
+int cameras_connect();
+void cameras_disconnect();
+std::array<long int, 4> cameras_getROI(int index);
 std::string cameras_getGUID(int index);
 
-void   cameras_setROI(unsigned int width, unsigned int height,
-			unsigned int offsetX, unsigned int offsetY, int index = -1); 
+void cameras_setROI(unsigned int width, unsigned int height,
+                    unsigned int offsetX, unsigned int offsetY, int index = -1);
 
-void   cameras_setFPS(float fps, bool for_trigger);
+void cameras_setFPS(float fps, bool for_trigger);
 
-void   cameras_start(int numFrames = 0 ); 
-void   cameras_stop(); 
+void cameras_start(int numFrames = 0);
+void cameras_stop();
 
-void   cameras_grab(std::string path, int obs_frames, int rec_frames, bool motion_act, bool directSave, bool trigger,bool strobe, int liveStream[], int enCams[], int streamCam); 
-void   cameras_save(std::string path); 
-
+void cameras_grab(std::string path, int obs_frames, int rec_frames, bool motion_act,
+                  bool directSave, bool trigger, bool strobe, int liveStream[],
+                  int enCams[], int streamCam);
+void cameras_save(std::string path);
